834b6fa6023e38125bd5a2feb1c23411c6698093
Pangaud <117455241+Pangaudl@users.noreply.github.com>
2022-11-14 10:01:25 +0000

    Initial commit

---
+++ Point/PointA.java
@@ -0,0 +1,8 @@
+package Point;
+
+public class PointA extends Point{
+
+  public void afficher(){
+    System.out.println("x =" + getX(), "Y =" + getY());
+ }
+}
---
+++ TestMetiers/Personne.java
@@ -0,0 +1,11 @@
+package TestMetiers;
+
+public class Personne{
+
+  
+
+
+
+
+  
+}
